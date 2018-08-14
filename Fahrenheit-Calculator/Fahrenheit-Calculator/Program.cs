42c403c54a09b0a56fa4300e85b597b3fc2819e3
Alexander Møller <leroikier@hotmail.com>
2018-08-14 18:14:56 +0200

    <Programmet færdig gøres med kommentar>

--- Fahrenheit-Calculator/Fahrenheit-Calculator/Program.cs
+++ Fahrenheit-Calculator/Fahrenheit-Calculator/Program.cs
@@ -10,13 +10,17 @@ namespace Fahrenheit_Calculator
     {
         static void Main(string[] args)
         {
+            //Input
             Console.WriteLine("Indtast Fahrenheit");
-            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = double.Parse(Console.ReadLine());
 
+            //Udregning
             double celcius = (fahrenheit - 32) * 5 / 9;
 
+            //Output
             Console.WriteLine("Fahrenheit til Celcius = {0}",celcius);
 
+            //Slut
             Console.ReadKey();
         }
     }
