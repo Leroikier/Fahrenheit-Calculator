0423ff02b662b0290e374a1c43c270bfcbdf7c08
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-14 14:40:16 +0200

    <Opgave 1.1 Fahrenheit til Celcius>

---
+++ Fahrenheit-Calculator/Fahrenheit-Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrenheit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast Fahrenheit");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            double celcius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("Fahrenheit til Celcius = {0}",celcius);
+
+            Console.ReadKey();
+        }
+    }
+}
